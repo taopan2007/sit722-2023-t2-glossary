f6556d2d4de84e5260359c72634bbbc025e31a08
Tao Pan <taopan2007@gmail.com>
2023-07-31 11:59:21 +0930

    assignment update

--- routes/terms.js
+++ routes/terms.js
@@ -75,6 +75,76 @@ const termsList = [
         "Definition": "Continued effort over a long period. Needed to fully transform culture and processes for DevOps.",
         "Source": "Mulesoft (2017) How to persuade your boss to invest in DevOps. TechBeacon. Available at: ",
         "url": "https://techbeacon.com/app-dev-testing/how-persuade-boss-invest-devops-and-why-it-s-worth-it"
+    },
+    {
+        "ID": 11,
+        "Term": "Continuous Delivery (CD)",  
+        "Definition": "The practice of automating the entire software release process, from code changes to deployment to production, enabling rapid and reliable delivery of software.",
+        "Source": "Red Hat. (n.d.). What is CI/CD? Red Hat. Retrieved February 27, 2023,",
+        "url": "https://www.redhat.com/en/topics/devops/what-is-ci-cd"
+    },
+    {
+        "ID": 12,
+        "Term": "Deployment Pipeline",
+        "Definition": "An automated implementation of the software delivery process, allowing code changes to flow sequentially through build, test, and deploy stages. Enables fast, reliable releases.",
+        "Source": "Atlassian. (n.d.). Continuous Delivery. Atlassian. Retrieved February 27, 2023,",
+        "url": "https://www.atlassian.com/continuous-delivery"
+    },
+    {
+        "ID": 13,
+        "Term": "Trunk-Based Development",
+        "Definition": "A source code management practice where developers collaborate by merging code frequently into a single shared branch (trunk) rather than maintaining isolated branches.",
+        "Source": "Atlassian. (n.d.). Trunk Based Development. Atlassian. Retrieved February 27, 2023,",
+        "url": "https://www.atlassian.com/continuous-delivery/continuous-integration/trunk-based-development"
+    },
+    {
+        "ID": 14,
+        "Term": "Infrastructure as Code (IaC)",
+        "Definition": "Managing and provisioning infrastructure like servers, networks, and configurations through machine-readable definition files rather than manual processes. Enables consistency and automation.",
+        "Source": "Red Hat. (n.d.). What is Infrastructure as Code. Red Hat. Retrieved February 27, 2023,",
+        "url": "https://www.redhat.com/en/topics/automation/what-is-infrastructure-as-code-iac"
+    },
+    {
+        "ID": 15,
+        "Term": "Configuration Drift",
+        "Definition": "When infrastructure or system configurations change or \"drift\" from their original settings over time, leading to inconsistencies and issues. IaC helps prevent this.",
+        "Source": "Red Hat. (n.d.). What is Configuration Drift. Red Hat. Retrieved February 27, 2023,",
+        "url": "https://www.redhat.com/en/topics/automation/what-is-configuration-drift"
+    },
+    {
+        "ID": 16, 
+        "Term": "Value Stream",
+        "Definition": "The set of activities, from concept to customer, required to deliver a product or service. Identifying and optimizing value streams is key in DevOps.",
+        "Source": "Atlassian. (n.d.). Value Stream Mapping. Atlassian. Retrieved February 27, 2023,",
+        "url": "https://www.atlassian.com/team-playbook/plays/value-stream-mapping"
+    },
+    {
+        "ID": 17,
+        "Term": "Feedback Loops",
+        "Definition": "Building mechanisms to get fast feedback on software changes. Tight feedback loops (e.g. CI/CD) enable continuous improvement.",
+        "Source": "Amazon Web Services. (n.d.). Implement Feedback Loops. Amazon Web Services. Retrieved February 27, 2023,",
+        "url": "https://docs.aws.amazon.com/whitepapers/latest/organizational-excellence/feedback-loops.html"
+    },
+    {
+        "ID": 18,
+        "Term": "Postmortems",
+        "Definition": "Retrospective analysis done after an incident, outage, or failure to identify root causes and improve processes. Key DevOps practice.",
+        "Source": "Atlassian. (n.d.). Blameless Postmortems. Atlassian. Retrieved February 27, 2023,", 
+        "url": "https://www.atlassian.com/incident-management/postmortem/blameless"
+    },
+    {
+        "ID": 19,
+        "Term": "Technical Debt",
+        "Definition": "Problems in code or architecture that accumulate over time as teams take shortcuts. Must be addressed for quality and velocity.",
+        "Source": "Atlassian. (n.d.). Technical Debt. Atlassian. Retrieved February 27, 2023,",
+        "url": "https://www.atlassian.com/continuous-delivery/principles/technical-debt"
+    },
+    {
+        "ID": 20,
+        "Term": "Velocity",
+        "Definition": "A measure of a team's rate of development, often measured in story points completed per sprint. Improves with DevOps.",
+        "Source": "Atlassian. (n.d.). Velocity. Atlassian. Retrieved February 27, 2023,",
+        "url": "https://www.atlassian.com/agile/project-management/velocity"
     }
 ];
 
